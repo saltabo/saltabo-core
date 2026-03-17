5cc49b1238527f9f09fb220798af27f547c0a3bc
tonypham <tunglamldk@gmail.com>
2026-03-17 14:20:36 +0700

    Initial Commit

---
+++ SwitchTab/SwitchTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchTabApp.swift
+//  SwitchTab
+//
+//  Created by Lam Tung on 17/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SwitchTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
